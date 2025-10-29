b49cb98354b40fa42a1b93f3b7988eff902880ec
JozilovaZ <jozilovazuxra8899@gmail.com>
2025-10-29 12:03:55 +0500

    loading

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ window.addEventListener("scroll",function(){
     header.classList.remove("scrolled")
   }
 })
+
+const scrol=document.querySelector(".scrol");
+scrol.addEventListener("click", function(){
+
+})
+
+document.addEventListener("DOMContentLoaded",function(){
+  const loader=document.querySelector(".loader");
+  setTimeout(()=>{
+    loader.style.opacity='0'; /* tiniqligi */
+    loader.style.display='none'
+  },4000)
+})
